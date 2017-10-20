3a7284a1dc8df830651a6ef958e3da456ded1be8
Joerg Flechsig <goergchen+github@gmail.com>
2017-10-20 22:52:32 +0200

    excluded legacy support from project

--- BlurryWindow/Helper/BlurryHelper.cs
+++ BlurryWindow/Helper/BlurryHelper.cs
@@ -2,10 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
-using System.Windows.Media;
 using BlurryControls.Internals;
-//using BlurryControls.Properties;
-//using OperatingSystem = BlurryControls.Internals.OperatingSystem;
 
 namespace BlurryControls.Helper
 {
@@ -13,42 +10,13 @@ namespace BlurryControls.Helper
     {
         public static void Blur(this Window win)
         {
-            EnableBlur10(win, true);
-
-            //switch (VersionHelper.OperatingSystem)
-            //{
-            //    case OperatingSystem.Windows78:
-            //        EnableBlur78(win, true);
-            //        break;
-            //    case OperatingSystem.Windows10:
-            //        EnableBlur10(win, true);
-            //        break;
-            //    case OperatingSystem.NotSupported:
-            //        throw new NotSupportedException(Resources.VersionNotSupported);
-            //    default:
-            //        throw new ArgumentOutOfRangeException();
-            //}
+            EnableBlur(win, true);
         }
+
         public static void UnBlur(this Window win)
         {
-            EnableBlur10(win, false);
-
-            //switch (VersionHelper.OperatingSystem)
-            //{
-            //    case OperatingSystem.Windows78:
-            //        EnableBlur78(win, false);
-            //        break;
-            //    case OperatingSystem.Windows10:
-            //        EnableBlur10(win, false);
-            //        break;
-            //    case OperatingSystem.NotSupported:
-            //        throw new NotSupportedException(Resources.VersionNotSupported);
-            //    default:
-            //        throw new ArgumentOutOfRangeException();
-            //}
+            EnableBlur(win, false);
         }
-
-        #region Windows10
 
         [DllImport("user32.dll")]
         internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
@@ -59,7 +27,7 @@ namespace BlurryControls.Helper
         /// <summary>
         /// this method uses the SetWindowCompositionAttribute to apply an AeroGlass effect to the window
         /// </summary>
-        private static void EnableBlur10(Window win, bool enable)
+        private static void EnableBlur(Window win, bool enable)
         {
             //this code is taken from a sample application provided by Rafael Rivera
             //see the full code sample here: (2016/07)
@@ -87,37 +55,5 @@ namespace BlurryControls.Helper
 
             Marshal.FreeHGlobal(accentPtr);
         }
-
-        #endregion
-
-        #region Windows78
-
-        /// <summary>
-        /// this method uses DwmEnableBlurBehindWindow to apply an AeroGlass effect to the window
-        /// </summary>
-        [Obsolete("not working without a window style and as such redundant")]
-        private static void EnableBlur78(Window win, bool enable)
-        {
-            //this code is taken from a sample provided by MSDN
-            //see https://msdn.microsoft.com/en-us/library/ms748975.aspx (2016/10)
-            var mainWindowPtr = new WindowInteropHelper(win).Handle;
-            var mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
-            if (mainWindowSrc == null) return;
-            if (mainWindowSrc.CompositionTarget != null)
-                mainWindowSrc.CompositionTarget.BackgroundColor = ((SolidColorBrush)win.Background).Color;
-            var margins = new MARGINS
-            {
-                // If any of the margins is "-1" the whole window is glass!
-                cxLeftWidth = enable ? -1 : 0,
-                cxRightWidth = 0,
-                cyBottomHeight = 0,
-                cyTopHeight = 0
-            };
-
-            var hr = DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
-            if (hr < 0) { /* DwmExtendFrameIntoClientArea Failed */ }
-        }
-
-        #endregion
     }
 }
