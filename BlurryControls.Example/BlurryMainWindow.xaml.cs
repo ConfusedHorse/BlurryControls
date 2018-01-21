04f73998f1355cbf1efab05c20ef96e5f931a81d
Joerg Flechsig <goergchen+github@gmail.com>
2018-01-21 02:14:16 +0100

    refactoring: minor adjustments

--- BlurryControls.Example/BlurryMainWindow.xaml.cs
+++ BlurryControls.Example/BlurryMainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using BlurryControls.Controls;
 using BlurryControls.DialogFactory;
 using BlurryControls.Example.Resources;
 
@@ -9,7 +8,7 @@ namespace BlurryControls.Example
     /// Interaction logic for BlurryMainWindow.xaml
     /// Contains some simple logic to demonstrate the BlurryWindow control
     /// </summary>
-    public partial class BlurryMainWindow : BlurryWindow
+    public partial class BlurryMainWindow
     {
         public BlurryMainWindow()
         {
@@ -18,11 +17,7 @@ namespace BlurryControls.Example
 
         private void HelloMenuBarButton_OnClick(object sender, RoutedEventArgs e)
         {
-            // TODO auto define width
-            // TODO auto overflow
-            // TODO Ok button is fallback
             BlurryMessageBox.Show(this, Lorem.Ipsum, "Hello");
-            //BlurryMessageBox.Show(this, "You did the thing.", "Hello");
         }
     }
 }
