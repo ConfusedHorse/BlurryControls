73a82c69d2e20b42d227475a073fd98e7e354d8b
Joerg Flechsig <goergchen+github@gmail.com>
2018-02-13 23:50:35 +0100

    development: added BlurrySlider

---
+++ BlurryControls/Controls/BlurrySlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace BlurryControls.Controls
+{
+    public class BlurrySlider : Slider
+    {
+        private Thumb _thumb;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (_thumb != null) _thumb.MouseEnter -= Thumb_MouseEnter;
+            _thumb = (GetTemplateChild("PART_Track") as Track)?.Thumb;
+            if (_thumb != null) _thumb.MouseEnter += Thumb_MouseEnter;
+        }
+        
+        private static void Thumb_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed) return;
+            var args = new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left)
+            {
+                RoutedEvent = MouseLeftButtonDownEvent
+            };
+            (sender as Thumb)?.RaiseEvent(args);
+        }
+    }
+}
